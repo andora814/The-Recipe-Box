fa4444e9849de408f5be0d244925c8943771add0
andora814 <smith.doran@gmail.com>
2022-02-08 12:13:10 -0500

    READ working for recipe on front end

---
+++ recipe_box_vue/src/components/RecipeCard.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="recipe_container">
+    <div class="container">
+      <div> {{ recipe.title }} </div>
+      <!-- <img :src="image_card.img" alt="picture" :class="picclass" @mouseover="mouseon" @mouseleave="mouseoff"/> -->
+      <!-- <div v-if="showName" class="center" >{{ image_card.title }}</div> -->
+    </div>
+    <!-- <div @click="deleteImage" class="delBtn">x</div> -->
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+export default {
+  name: 'RecipeCard',
+  data: () => ({
+    showName: false,
+    picclass: 'normal'
+  }),
+  props: {
+    recipe: Object
+  },
+  methods: {
+    // async deleteImage() {
+    //     await axios.delete(`http://localhost:8000/images/${this.image_card.id}`, {
+    //       auth: {
+    //         username: 'picmeuser',
+    //         password: 'picme'
+    //       }
+    //     })
+    //     this.$emit('handleDelete', this.image_card.id)
+    },
+    // mouseon () {
+    //   this.showName = true
+    //   this.picclass = 'blur'
+    // },
+    // mouseoff () {
+    //   this.showName = false
+    //   this.picclass = 'normal'
+    // }
+  // }
+}
+</script>
+
+<style scoped>
+
+img {
+  max-height: 200px;
+  border-radius: 7px 7px 0 0;
+}
+
+.container {
+  position: relative;
+  display: flex;
+  justify-content: center;
+}
+
+.center {
+  position: absolute;
+  top: 50%;
+  text-align: center;
+  font-size: 22px;
+  color: black
+}
+
+img { 
+  width: 100%;
+  height: auto;
+}
+
+.blur {
+ opacity: 0.6;
+}
+
+.image_card_container {
+  width: 300px;
+  margin-bottom: 10px;
+}
+
+.delBtn {
+  width: 100%;
+  background-color: #80cbc4;
+  font-size: 20px;
+  font-weight: 700;
+  cursor: pointer;
+  border-radius: 0 0 7px 7px
+}
+
+.delBtn:hover {
+  background-color: #682424;
+  color: white
+}
+
+
+</style>
--- recipe_box_vue/src/pages/Home.vue
+++ recipe_box_vue/src/pages/Home.vue
@@ -1,10 +1,34 @@
 <template>
-    <div>This is Home</div>
+    <div class="recipe-container-grid">
+        <div v-for="recipe in recipe_array" :key="recipe.id">
+            <RecipeCard v-bind:recipe="recipe" />
+        </div>
+    </div>
 </template>
 
 <script>
+import RecipeCard from '../components/RecipeCard.vue'
+import axios from 'axios'
 export default {
-
+    name: 'Home',
+    components: {
+        RecipeCard
+    },
+    data: () => ({
+        recipe_array: []
+    }),
+    mounted() {
+        this.getRecipes()
+    },
+    methods: {
+        async getRecipes() {
+            console.log("mounted function is working")
+            const res = await axios.get(`http://localhost:8000/recipes`)
+            console.log(res.data)
+            this.recipe_array = res.data
+        }
+    }
+    
 }
 </script>
 
