6b6d34e2e3aa663ce8082cd4eace388cd25657ff
andora814 <smith.doran@gmail.com>
2022-02-10 14:33:54 -0500

    home page rendering existing ingredients, and posting new ingredients using external api search bar

--- recipe_box_vue/src/pages/Home.vue
+++ recipe_box_vue/src/pages/Home.vue
@@ -1,13 +1,20 @@
 <template>
-    <div>This is Home.
-    <input placeholder="Search food" name="food_search" type="food_search" v-on:input="handleFormChange" class="food-search"/>
+    <div>
+    <input placeholder="Search food" name="search_word" type="food_search" v-on:input="handleFormChange" class="food-search"/>
             <div>
-                <!-- <div> {{ food.name }} </div> -->
-                <div v-for="food in food_search" :key="food.id">
-                    <div> {{ food.name }} </div>
-                    <button @click="SelectFood(food.name)">Select food</button>
+                <div v-if="show_food_search">
+                    <div v-for="food in food_search" :key="food.id">
+                        <div> {{ food.name }} </div>
+                        <button @click="SelectFood(food.name)">Select food</button>
+                    </div>
                 </div>
             </div>
+    <button @click="ExternalFoodSearch(search_word)">Search</button>
+    <div>
+        <div v-for="ingredient in all_ingredients" :key="ingredient.id">
+            {{ ingredient.name }}
+        </div>
+    </div>
     </div>
 </template>
 
@@ -22,20 +29,23 @@ export default {
     },
     data: () => ({
         food_search: [],
-        show_food_search: false
+        show_food_search: false,
+        search_word: '',
+        all_ingredients: []
     }),
     mounted() {
-  
+        this.GetAllFood()
     },
     methods: {
         handleFormChange(e) {
             this[e.target.name] = e.target.value
-            if(e.target.value.length >= 3) {
-                this.show_food_search=true
-                this.ExternalFoodSearch(this.food_search)
-            }
+            // if(e.target.value.length >= 3) {
+                // this.show_food_search=true
+                // this.ExternalFoodSearch(this.food_search)
+            // }
         },
         async ExternalFoodSearch (keyword) { 
+            this.show_food_search = true
             const res = await axios.get(`https://api.spoonacular.com/food/ingredients/search?query=${keyword}&number=20&sort=calories&sortDirection=desc&apiKey=9e2e99bbc2e8487baf824d1acd1621e9`);
             this.food_search = res.data.results
             console.log(this.food_search)
@@ -50,6 +60,11 @@ export default {
                 }
             })
             this.show_food_search=false
+        },
+        async GetAllFood() {
+            const response = await axios.get('http://localhost:8000/ingredients/'
+            )
+            this.all_ingredients =response.data
         }
     }
 }
